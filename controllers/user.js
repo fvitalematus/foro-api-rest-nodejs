19ebe57004ba43cf83831bed14f3ea7a539213b1
Franco Vitale <franco.vitalematus@gmail.com>
2020-07-09 04:10:22 -0400

    Mostrar Usuarios: Avatar y Listas

--- controllers/user.js
+++ controllers/user.js
@@ -260,7 +260,7 @@ var controller = {
         }
 
         // Conseguir el nombre y la extension del archivo.
-        var file_path = req.files.path;
+        var file_path = req.files.file0.path;
         var file_split = file_path.split('\\');
 
         // Nombre del archivo
@@ -305,6 +305,57 @@ var controller = {
             });
         }
 
+    },
+
+    avatar: function (req, res) {
+        var fileName = req.params.fileName;
+        var pathFile = './uploads/users/' + fileName;
+
+        fs.exists(pathFile, (exists) => {
+            if (exists) {
+                return res.sendFile(path.resolve(pathFile));
+            } else {
+                return res.status(404).send({
+                    message: 'La imagen no existe'
+                });
+            }
+        });
+    },
+
+    // Metodos getUsers y getUser
+    getUsers: function (req, res) {
+        User.find().exec((err, users) => {
+            if (err || !users) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay usuarios que mostrar'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                users
+            });
+
+        });
+    },
+
+    getUser: function (req, res) {
+        var userId = req.params.userId;
+
+        User.findById(userId).exec((err, user) => {
+            if (err || !user) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No existe el usuario'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                user
+            });
+        });
     }
 
 };
--- routes/user.js
+++ routes/user.js
@@ -18,6 +18,9 @@ router.post('/register', UserController.save);
 router.post('/login', UserController.login);
 router.put('/update', md_auth.authenticated, UserController.update); // middleware auth
 router.post('/upload-avatar', [md_auth.authenticated, md_upload], UserController.uploadAvatar);
+router.get('/avatar/:fileName', UserController.avatar);
+router.get('/users', UserController.getUsers);
+router.get('/user/:userId', UserController.getUser);
 
 
 module.exports = router;
